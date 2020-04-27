bcd84c3419f46e1fd496834b0b0148986c4a96fb
Pablo Andrade <46230565+pablo-vieira-59@users.noreply.github.com>
2020-04-27 16:56:50 -0300

    Updated webcam capture

--- Web_Server/static/js/script.js
+++ Web_Server/static/js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var apple = document.getElementById("apple");
 var apple_icon = document.getElementById("apple-icon");
 var axe = document.getElementById("axe");
@@ -8,7 +10,33 @@ var book = document.getElementById("book");
 var book_icon = document.getElementById("book-icon");
 var bus = document.getElementById("bus");
 var bus_icon = document.getElementById("bus-icon");
-var cam_feed = document.getElementById("cam-feed");
+
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const snap = document.getElementById("snap");
+
+
+const constraints = {
+    audio: false,
+    video:{
+        width: 640, height: 480
+    }
+};
+
+async function init(){
+    try{
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+
+function handleSuccess(stream){
+    window.stream = stream;
+    video.srcObject = stream;
+}
 
 function setOnline(div, icon){
     div.style.color = "green";
@@ -21,36 +49,68 @@ function setOffline(div, icon){
 }
 
 function captureImage(){
-    var req = $.get('/data');
-    req.done( 
-        function (result) {
-            setOffline(apple, apple_icon);
-            setOffline(axe, axe_icon);
-            setOffline(bicycle, bicycle_icon);
-            setOffline(book, book_icon);
-            setOffline(bus, bus_icon);
+    context.drawImage(video, 0,0, 640, 480);
+    var imgURL = canvas.toDataURL();
+    $.ajax({
+        type: "POST",
+        url: "/load_camera",
+        data: {
+            image:imgURL
+        },
+        success: function(data) {
+          return;
+        },
+        error: function(data) {
+          alert('There was an error uploading your file!');
+        }
+      }).done(function (result) {
+        setOffline(apple, apple_icon);
+        setOffline(axe, axe_icon);
+        setOffline(bicycle, bicycle_icon);
+        setOffline(book, book_icon);
+        setOffline(bus, bus_icon);
 
-            if(result.class == 0){
-                setOnline(apple, apple_icon);
-            }
+        if(result.class == 0){
+            setOnline(apple, apple_icon);
+        }
 
-            if(result.class == 1){
-                setOnline(axe, axe_icon);
-            }
+        if(result.class == 1){
+            setOnline(axe, axe_icon);
+        }
 
-            if(result.class == 2){
-                setOnline(bicycle, bicycle_icon);
-            }
+        if(result.class == 2){
+            setOnline(bicycle, bicycle_icon);
+        }
 
-            if(result.class == 3){
-                setOnline(book, book_icon);
-            }
+        if(result.class == 3){
+            setOnline(book, book_icon);
+        }
 
-            if(result.class == 4){
-                setOnline(bus, bus_icon);
-            }
+        if(result.class == 4){
+            setOnline(bus, bus_icon);
+        }
 
-        });
+    });
 }
 
-cam_feed.src = "http://127.0.0.1/framed"
+function drawImge(){
+    context.drawImage(video, 0, 0, 640, 480);
+
+    var faceArea = 300;
+    var pX=canvas.width/2 - faceArea/2;
+    var pY=canvas.height/2 - faceArea/2;
+
+    context.rect(pX,pY,faceArea,faceArea);
+    context.lineWidth = "4";
+    context.strokeStyle = "red";    
+    context.stroke();
+
+    setTimeout(drawImge , 20);
+}
+
+init();
+var context = canvas.getContext('2d');
+video.hidden = true;
+video.onplay = function() {
+    setTimeout(drawImge , 20);
+};
--- Web_Server/web_server.py
+++ Web_Server/web_server.py
@@ -4,8 +4,10 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 import urllib.request as ur
 import json , requests, keras , cv2
-from flask import Flask, make_response, render_template, Request, jsonify, Response
+from flask import Flask, make_response, render_template, Request, jsonify, Response, request
 import tensorflow as tf
+import re
+import base64
 
 # Set CPU as available physical device
 tf.config.set_visible_devices([], 'GPU')
@@ -14,10 +16,18 @@ tf.config.set_visible_devices([], 'GPU')
 model = tf.keras.models.load_model('../Machine_Learning/saved_models/main_model.h5')
 
 # Rect Value
-start_x = 400 - 150
-start_y = 300 - 150
-end_x = 400 + 150
-end_y = 300 + 150
+frame_w = 640
+frame_h = 480
+box_size = 200
+
+fw_size = (frame_w//2)
+fh_size = (frame_h//2)
+b_size = box_size//2
+
+start_x = fw_size - b_size
+start_y = fh_size - b_size
+end_x = fw_size + b_size
+end_y = fh_size + b_size
 
 app = Flask(__name__)
 
@@ -44,12 +54,15 @@ def apply_frame(img):
     retval , frame = cv2.imencode('.png',img)
     return frame
 
+@app.route("/load_camera",methods=["GET","POST"])
 def load_camera():
-    url = "http://10.0.0.12:5959/photo.jpg"
-    image = ur.urlopen(url,timeout=2)
-    image = np.array(bytearray(image.read()),dtype=np.uint8)
-    image = cv2.imdecode(image,-1)
-    return image
+    data = request.values['image']
+    img = re.sub('^data:image/.+;base64,', '', data)
+    img = base64.b64decode(img)
+    img = np.frombuffer(img, dtype=np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    
+    return make_prediction(img)
 
 def process_image(img):
     # Recorta Imagem
@@ -106,10 +119,8 @@ def components():
 def display_frame():
     return Response(draw(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route("/data",methods=["GET","POST"])
-def make_prediction():
+def make_prediction(img):
     global model
-    img = load_camera()
     x_pred = process_image(img)
     pred = model.predict_classes(x_pred)
     return jsonify({'class':int(pred[0])})
@@ -118,5 +129,5 @@ def make_prediction():
 
 if __name__ == "__main__":  
       
-    app.run(debug=True,port=80,host="0.0.0.0")
+    app.run(debug=True,port=8080,host="0.0.0.0")
     
